e03bd7cb3826d2991d8147d5dc86e1e5867553f8
Ana Victoria <anav.hernandez@softtek.com>
2019-12-20 12:36:29 -0600

    primer comit de filesystem

---
+++ FilesSystem/SqlDependency/Customers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SqlDependency
+{
+    public class Customers
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+
+        public string  Name { get; set; }
+
+        public string Surname { get; set; }
+
+        //Properties not prsent in db table are ignored
+
+        public string City { get; set; }
+
+        public DateTime Born { get; set; }
+    }
+}
---
+++ FilesSystem/SqlDependency/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.Enums;
+using TableDependency.SqlClient.Base.EventArgs;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace SqlDependency
+{
+    class Program
+    {
+        public static void SerializeToXml<T>(T obj, string fileName)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+            //Create a filestream object connected to the target file
+
+            FileStream fileStream = new FileStream(fileName, FileMode.Create);
+            ser.Serialize(fileStream, obj);
+            fileStream.Close();
+        }
+
+        public static T DeserializeFromXml<T>(string xml)
+        {
+            T result;
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+            using (TextReader tr = new StringReader(xml))
+            {
+                result = (T)ser.Deserialize(tr);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string connectionString = @"data source =LSTK231296\SQLEXPRESS ; initial catalog=prueba ; integrated security = True";
+            using (TableDependency.SqlClient.SqlTableDependency<Customers> dep = new TableDependency.SqlClient.SqlTableDependency<Customers>(connectionString))
+            {
+                dep.OnChanged += Dep_OnChanged;
+
+                dep.Start();
+                Console.WriteLine("Waiting for receiving notificatiions...");
+                Console.WriteLine("Press a key to stop");
+                Console.ReadKey();
+                dep.Stop();
+            }
+        }
+
+        static void Dep_OnChanged(object sender, RecordChangedEventArgs<Customers> e)
+        {
+            //Si es INSERT
+            if (e.ChangeType.ToString() == "Insert")
+            {
+                List<Customers> ListaCustomers = new List<Customers>();
+                if (File.Exists(@"C:\Users\Curso\source\repos\FilesSystem\Customers.xml"))
+                {
+                    XmlDocument Doc = new XmlDocument();
+                    Doc.Load(@"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+                    ListaCustomers.AddRange(DeserializeFromXml<List<Customers>>(Doc.OuterXml));
+                }
+
+                Customers lCustomers = new Customers();
+                lCustomers.Id = e.Entity.Id;
+                lCustomers.Name = e.Entity.Name;
+                lCustomers.Surname = e.Entity.Surname;
+
+                ListaCustomers.Add(lCustomers);
+                SerializeToXml(ListaCustomers, @"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+                //SerializeToXml<List<Customers>>(ListaCustomers, @"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            //Si es Delete
+            if (e.ChangeType.ToString() == "Delete")
+            {
+                XElement Doc = XElement.Load(@"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+
+                foreach (XNode item in Doc.Nodes())
+                {
+                    var idAttribute = ((XElement)item).Attributes("Id").FirstOrDefault().Value;
+                    if (idAttribute == e.Entity.Id.ToString())
+                    {
+                        item.Remove();
+                        Doc.Save(@"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+                    }
+                }
+            }
+
+            //Si es Update
+            if (e.ChangeType.ToString() == "Update")
+            {
+                XElement Doc = XElement.Load(@"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+
+                foreach (XElement item in Doc.Nodes())
+                {
+                    var idAttribute = item.Attributes("Id").FirstOrDefault().Value;
+                    if (idAttribute == e.Entity.Id.ToString())
+                    {
+                        item.SetElementValue("Name", e.Entity.Name.ToString());
+                        item.SetElementValue("Surname", e.Entity.Surname.ToString());
+
+                        Doc.Save(@"C:\Users\Curso\source\repos\FilesSystem\Customers.xml");
+                    }
+                }
+            }
+
+            //si hubo un cambio en la tabla
+            if (e.ChangeType != ChangeType.None)
+            {
+                Console.WriteLine("DML operation: " + e.ChangeType);
+                Console.WriteLine(e.Entity.Id);
+                Console.WriteLine(e.Entity.Name);
+                Console.WriteLine(e.Entity.Surname);
+                Console.WriteLine(Environment.NewLine);
+            }
+        }
+    }
+}
