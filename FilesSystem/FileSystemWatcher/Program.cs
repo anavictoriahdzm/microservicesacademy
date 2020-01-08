e7c7105e531464f8c72a2981be7f74ac0a48aa9e
Ana Victoria <anav.hernandez@softtek.com>
2020-01-07 18:57:54 -0600

    AppShoesMVC aun falta

--- FilesSystem/FileSystemWatcher/Program.cs
+++ FilesSystem/FileSystemWatcher/Program.cs
@@ -5,12 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace FileSystemWatcher
+namespace FileSystem
 {
     class Program
     {
         static void Main(string[] args)
         {
+            FileSystemWatcher observador = new FileSystemWatcher(@"C:\Users\Curso\source\repos\FilesSystem");
+            observador.NotifyFilter = (
+                NotifyFilters.LastAccess |
+                NotifyFilters.LastWrite|
+                NotifyFilters.FileName |
+                NotifyFilters.DirectoryName);
+
+            observador.Changed += AlCambiar;
+            observador.Created += AlCambiar;
+            observador.Deleted += AlCambiar;
+            observador.Renamed += AlRenombrar;
+            observador.Error += AlOcurrirUnError;
+
+            observador.EnableRaisingEvents = true;
+            Console.WriteLine("Presiona enter para detener");
+            Console.ReadLine(); 
+        }
+
+        private static void AlRenombrar(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("El archivo {0} ahora se llama {1}", e.OldFullPath, e.FullPath);
+        }
+
+        private static void AlCambiar(object sender, FileSystemEventArgs e) 
+        {
+            WatcherChangeTypes tipoDeCambio = e.ChangeType;
+            Console.WriteLine("El archivo {0} tuvo cambio de: {1}", e.FullPath, tipoDeCambio.ToString());
+        }
+
+        private static void AlOcurrirUnError(object source, ErrorEventArgs e)
+        {
+            Console.WriteLine("Error; " + e.GetException().Message);
         }
     }
 }
---
+++ ShoesAppMVC/Data/ModeloProducto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ModeloProducto
+    {
+        public int Id { get; set; }
+        public Nullable<int> IdType { get; set; }
+        public Nullable<int> IdColor { get; set; }
+        public Nullable<int> IdBrand { get; set; }
+        public Nullable<int> IdProvider { get; set; }
+        public int IdCatalog { get; set; }
+        public string Title { get; set; }
+        public string Nombre { get; set; }
+        public string Description { get; set; }
+        public string Observations { get; set; }
+        public Nullable<decimal> PriceDistributor { get; set; }
+        public decimal PriceClient { get; set; }
+        public decimal PriceMember { get; set; }
+        public bool IsEnabled { get; set; }
+        public string Keywords { get; set; }
+        public System.DateTime DateUpdate { get; set; }
+
+        public virtual CatBrand CatBrand { get; set; }
+        public virtual CatCatalog CatCatalog { get; set; }
+        public virtual CatColor CatColor { get; set; }
+        public virtual CatProvider CatProvider { get; set; }
+        public virtual CatTypeProduct CatTypeProduct { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Comment> Comments { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<DetailProduct> DetailProducts { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ImagesProduct> ImagesProducts { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SimilarProduct> SimilarProducts { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SizeForProduct> SizeForProducts { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Qualification> Qualifications { get; set; }
+    }
+}
