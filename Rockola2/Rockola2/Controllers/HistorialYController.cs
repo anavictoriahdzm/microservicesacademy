f2b204ae1f3d32daf6932eafe48ecc69d6ff0717
AnaHernandez <anav.hernandez@softtek.com>
2020-02-17 10:28:25 -0600

    se agrego la vista del historial

---
+++ Rockola2/Rockola2/Controllers/HistorialYController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Rockola2.Models;
+
+namespace Rockola2.Controllers
+{
+    public class HistorialYController : Controller
+    {
+        //URL donde se ubica nuestra API
+        string Baseurl = "http://localhost:7012/";
+
+        public async Task<ActionResult> Index()
+        {
+            List<HistorialY> HistorialInfo = new List<HistorialY>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //llama todo el contenido usando el HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/HistorialY/");
+                if(Res.IsSuccessStatusCode)
+                {
+                    //Si Rest = true entra y asigna los datos
+                    var HistorialResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializar el api y almacena los datos
+                    HistorialInfo = JsonConvert.DeserializeObject<List<HistorialY>>(HistorialResponse);
+                }
+                //Muestra la lista de todos los usuarios
+                return View();
+            }
+        }
+    }
+}
---
+++ Rockola2/Rockola2/Models/HistorialY.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rockola2.Models
+{
+    public class HistorialY
+    {
+        public int id { get; set; }
+        public string videoName { get; set; }
+        public string videoId { get; set; }
+    }
+}
