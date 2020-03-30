51a42c641cd1f898bd09e3520392224f265b0273
AnaHernandez <anav.hernandez@softtek.com>
2020-03-29 19:30:34 -0600

    Practica de mail en core

---
+++ EmailCore/EmailCore/Email.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace EmailCore
+{
+    public class Emails
+    {
+        public static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(@"C:\Users\Curso\Desktop\Test");
+
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+
+            //add event hanlders
+            watcher.Renamed += watcher_Renamed;
+            Console.Read();
+        }
+
+        public static void watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("se enviara correo");
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+
+            mmsg.To.Add("softtekt4@gmail.com");
+            mmsg.Subject = "Prueba";
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            mmsg.Body = "Es una prueba";
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = true;
+            mmsg.From = new System.Net.Mail.MailAddress("softtekt4@gmail.com");
+
+            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+            cliente.Credentials = new System.Net.NetworkCredential("softtekt4@gmail.com", "Soporte01");
+
+            cliente.Port = 587;
+            cliente.EnableSsl = true;
+            cliente.Host = "smtp.gmail.com";
+
+            try
+            {
+                cliente.Send(mmsg);
+            }
+            catch (Exception i)
+            {
+
+                Console.WriteLine("Error: " + i);
+            }
+
+        }
+    }
+}
