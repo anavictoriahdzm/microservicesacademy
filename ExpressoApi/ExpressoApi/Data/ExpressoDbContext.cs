a0a96787890c5268709a72e11bc970c2afee61b0
AnaHernandez <anav.hernandez@softtek.com>
2020-04-02 23:50:38 -0600

    Practica Expresso Api de netCore completa y funcionando

---
+++ ExpressoApi/ExpressoApi/Data/ExpressoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ExpressoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressoApi.Data
+{
+    public class ExpressoDbContext : DbContext
+    {
+        public ExpressoDbContext(DbContextOptions<ExpressoDbContext>option):base(option)
+        {
+
+        }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
