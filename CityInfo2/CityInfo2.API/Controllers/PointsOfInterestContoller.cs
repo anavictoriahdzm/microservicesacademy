aaa0790fbbc927e913fa85a0ad004f4b07147f2c
AnaHernandez <anav.hernandez@softtek.com>
2020-05-21 15:21:53 -0500

    funciona todo bien, hasta swagger

---
+++ CityInfo2/CityInfo2.API/Controllers/PointsOfInterestContoller.cs
@@ -0,0 +1,190 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.JsonPatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using CityInfo.API.Services;
+using AutoMapper;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestContoller : ControllerBase
+    {
+        private readonly ILogger<PointsOfInterestContoller> _logger;
+        private readonly IMailService _mailService;
+        private readonly ICityInfoRepository _cityInfoRepository;
+        private readonly IMapper _mapper;
+
+        public PointsOfInterestContoller(ILogger<PointsOfInterestContoller> logger,
+            IMailService mailService, ICityInfoRepository cityInfoRepository,
+            IMapper mapper)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _mailService = mailService ?? throw new ArgumentException(nameof(mailService));
+
+            _cityInfoRepository = cityInfoRepository ??
+                throw new ArgumentNullException(nameof(cityInfoRepository));
+
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public IActionResult GetPointsOfIInterest(int cityId)
+        {
+            try
+            {
+                //var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+                if(!_cityInfoRepository.CityExists(cityId))
+                {
+                    _logger.LogInformation($"City with Id {cityId} was not found when" + $"accessing points of interest.");
+                    return NotFound();
+                }
+
+                var pointsOfInterestForCity = _cityInfoRepository.GetPointsOfInterestForCity(cityId);
+                return Ok(_mapper.Map<IEnumerable<PointOfInterestDto>>(pointsOfInterestForCity));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points of interest for city with id {cityId}.", ex);
+                return StatusCode(500, "A problem happend while handling your request.");
+            }
+            
+        }
+
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
+        public IActionResult GetpointOfInterest(int cityId, int id)
+        {
+            if (!_cityInfoRepository.CityExists(cityId))
+                return NotFound();
+
+            var pointOfInterest = _cityInfoRepository.GetPointOfInterestForCity(cityId, id);
+
+            if (pointOfInterest == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<PointOfInterestDto>(pointOfInterest));
+        }
+
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest.Description == pointOfInterest.Name)
+                ModelState.AddModelError("Description",
+                    "The provided descriptionshould be different from the name");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_cityInfoRepository.CityExists(cityId))
+                return NotFound();
+
+            var finalPointOfInterest = _mapper.Map<Entities.PointOfInterest>(pointOfInterest);
+
+            _cityInfoRepository.AddPointOfInterestForCity(cityId, finalPointOfInterest);
+
+            _cityInfoRepository.Save();
+
+            var createdPointsOfInterestToReturn = _mapper
+                .Map<Models.PointOfInterestDto>(finalPointOfInterest);
+
+            return CreatedAtRoute(
+                "GetPointOfInterest",
+                new { cityId, id = createdPointsOfInterestToReturn.Id },
+                createdPointsOfInterestToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id,
+            [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest.Description == pointOfInterest.Name)
+                ModelState.AddModelError("Description",
+                    "the provided description should be different from the name.");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_cityInfoRepository.CityExists(cityId))
+                return NotFound();
+
+            var pointOfInterestEntity = _cityInfoRepository.GetPointOfInterestForCity(cityId, id);
+            if (pointOfInterestEntity == null)
+                return NotFound();
+
+            _mapper.Map(pointOfInterest, pointOfInterestEntity);
+
+            _cityInfoRepository.UpdatePointOfInterestForCity(cityId, pointOfInterestEntity);
+
+            _cityInfoRepository.Save();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (!_cityInfoRepository.CityExists(cityId))
+                return NotFound();
+
+            var pointOfInterestEntity = _cityInfoRepository.GetPointOfInterestForCity(cityId, id);
+            if (pointOfInterestEntity == null)
+                return NotFound();
+
+            var pointOfInterestToPatch = _mapper
+                .Map<PointOfInterestForUpdateDto>(pointOfInterestEntity);
+
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (pointOfInterestToPatch.Description == pointOfInterestToPatch.Name)
+                ModelState.AddModelError(
+                    "Description",
+                    "The provided description should be differente from the name.");
+
+            if (!TryValidateModel(pointOfInterestToPatch))
+                return BadRequest(ModelState);
+
+            _mapper.Map(pointOfInterestToPatch, pointOfInterestEntity);
+
+            _cityInfoRepository.UpdatePointOfInterestForCity(cityId, pointOfInterestEntity);
+
+            _cityInfoRepository.Save();
+
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            if (!_cityInfoRepository.CityExists(cityId))
+                return NotFound();
+
+            var pointOfInterestEntity = _cityInfoRepository
+                .GetPointOfInterestForCity(cityId, id);
+
+            if (pointOfInterestEntity == null)
+                return NotFound();
+
+            _cityInfoRepository.DeletePointOfInterest(pointOfInterestEntity);
+
+            _cityInfoRepository.Save();
+
+            _mailService.Send("Point of interest deleted.",
+                $"Point of interest {pointOfInterestEntity.Name} with id {pointOfInterestEntity.Id} was deleted.");
+
+            return NoContent();
+        }
+    }
+}
---
+++ CityInfo2/CityInfo2.API/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CityInfo.API.Context;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+
+namespace CityInfo.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration ??
+                throw new ArgumentNullException(nameof(configuration));
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //quitar el (;) para poder usar el codigo de abajo
+            services.AddMvc();
+
+            //Para que el texto lo regrese en XML
+            /*.AddMvcOptions(o =>
+            {
+                o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            });*=
+
+        //Para que el texto lo regrese en Json
+           /* .AddJsonOptions(O =>
+           {
+               if (O.SerializerSettings.ContractResolver != null)
+               {
+                   var castedResolver = O.SerializerSettings.ContractResolver
+                   as DefaultContractResolver;
+                   castedResolver.NamingStrategy = null;
+               }
+           });*/
+            //services.AddTransient<IMailService, LocalMailService>();
+
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            service.AddTransient<IMailService, CloudMailService>();
+#endif
+            var connectionString = _configuration["connectionStrings:cityInfoDBConnectionString"];
+            services.AddDbContext<CityInfoContext>(o =>
+            {
+                o.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "CityInfo2 API", Version = "v1" }));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else 
+            {
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CityInfo2 API"));
+        }
+    }
+}
